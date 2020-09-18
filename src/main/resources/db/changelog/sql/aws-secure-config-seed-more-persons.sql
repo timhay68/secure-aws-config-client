1e711ae10c382c26ef00745182ae4aefdda5defb
Tim Hay <tim.hay@auspost.com.au>
2020-09-19 09:11:12 +1000

    Added more seed data.

---
+++ src/main/resources/db/changelog/sql/aws-secure-config-seed-more-persons.sql
@@ -0,0 +1,15 @@
+INSERT INTO person
+(person_id, first_name, last_name, email, date_of_birth)
+VALUES (11111, 'Severus', 'Snape', 'severus@hogwarts.com.au', '1990-01-02');
+INSERT INTO person
+(person_id, first_name, last_name, email, date_of_birth)
+VALUES (11112, 'Harry', 'Potter', 'harry@hogwarts.com.au', '1990-01-03');
+INSERT INTO person
+(person_id, first_name, last_name, email, date_of_birth)
+VALUES (11113, 'Ginny', 'Weasley', 'ginny@hogwarts.com.au', '1990-01-04');
+INSERT INTO person
+(person_id, first_name, last_name, email, date_of_birth)
+VALUES (11114, 'Hermione', 'Granger', 'hermione@hogwarts.com.au', '1990-01-05');
+INSERT INTO person
+(person_id, first_name, last_name, email, date_of_birth)
+VALUES (11115, 'Ron', 'Weasley', 'ron@hogwarts.com.au', '1990-01-06');
